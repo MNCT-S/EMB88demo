676466cf69907c85ae37690084836f5dbddd4075
K-Magara <magara@maizuru-ct.ac.jp>
2020-11-27 16:27:42 +0900

    45度回転（#ifdef 使いすぎてコードが見にくいかも）

--- ExBoardSTM/main.c
+++ ExBoardSTM/main.c
@@ -11,18 +11,19 @@
 
 #define SW1_ON()	!(PINC & _BV(4))
 #define SW2_ON()	!(PINC & _BV(5))
-#define SLOW_MODE
+//#define SLOW_MODE
+#define DEG45		60		// 45deg/0.75
 
 enum MODE
 {
 	MODE1=0, MODE12=1, MODE2=2	
-}	mode = MODE12;				// pattern
+}	mode = MODE2;				// pattern
 unsigned char	pattern[][8] = {
 	{0b00010000, 0b00100000, 0b01000000, 0b10000000, 0b00010000, 0b00100000, 0b01000000, 0b10000000},
 	{0b00010000, 0b00110000, 0b00100000, 0b01100000, 0b01000000, 0b11000000, 0b10000000, 0b10010000},
 	{0b00110000, 0b01100000, 0b11000000, 0b10010000, 0b00110000, 0b01100000, 0b11000000, 0b10010000},
 };
-volatile int	ovf = 0, idx = 0, add = 0;
+volatile int	ovf = 0, idx = 0, add = 0, stm = 0;
 
 ////////
 
@@ -30,9 +31,12 @@ ISR(TIMER0_OVF_vect)
 {
 #ifdef SLOW_MODE
 	ovf++;
-	if ( ovf > 10 ) {
-#endif
+	if ( ovf > 10 && add != 0 ) {
 		ovf = 0;
+#else
+	if ( stm > 0 ) {
+		stm--;
+#endif
 		idx += add;
 		if ( idx > 7 )
 			idx = 0;
@@ -40,9 +44,7 @@ ISR(TIMER0_OVF_vect)
 			idx = 7;
 		PORTB &= 0x0f;
 		PORTB |= pattern[mode][idx];
-#ifdef SLOW_MODE
 	}
-#endif
 }
 
 int main(void)
@@ -58,23 +60,34 @@ int main(void)
 	PORTB = pattern[mode][idx];
 
 	// Timer0
+#ifdef SLOW_MODE
 	TCCR0B |= _BV(CS02) | _BV(CS00);	// 1/1024
-	TIMSK0 &= ~_BV(TOIE0);
+#else
+	TCCR0B |= _BV(CS02);				// 1/256
+#endif
+	TIMSK0 |= _BV(TOIE0);
 
 	sei();
     while (1) {
 		wdt_reset();
 		if ( SW1_ON() ) {
 			add = 1;
-			TIMSK0 |= _BV(TOIE0);
+#ifndef SLOW_MODE
+			if ( stm == 0 ) stm = DEG45;
+#endif
 		}
 		else if ( SW2_ON() ) {
 			add = -1;
-			TIMSK0 |= _BV(TOIE0);
+#ifndef SLOW_MODE
+			if ( stm == 0 ) stm = DEG45;
+#endif
 		}
 		else {
+#ifndef SLOW_MODE
+			if ( stm == 0 ) add = 0;
+#else
 			add = 0;
-			TIMSK0 &= ~_BV(TOIE0);
+#endif
 		}
     }
 }
--- README.md
+++ README.md
@@ -18,6 +18,7 @@ Author : Kenichi Magara / NIT, Maizuru College (KOSEN)
 オリジナル拡張ボードでステッピングモータの動作デモ  
 SW1で正回転，SW2で逆回転します  
 プログラム内の SLOW_MODE を有効にすると，人間の目にもわかるくらいのスピードで動きます  
+無効にすると45度ずつ動きます  
 1相励磁・1-2相励磁・2相励磁の切り替えは，mode 変数を変えてください  
 （動的に変えたかったがEMB-88の制約であきらめた）  
 Author : Kenichi Magara / NIT, Maizuru College (KOSEN)
